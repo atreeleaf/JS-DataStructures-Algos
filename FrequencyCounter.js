141bb2045494ed75cbce7e8d5d6ebe7cf89d7499
Andrew Tang <andrewtang@Andrews-MacBook-Pro.local>
2019-05-10 14:45:34 -0700

    updated instructions

--- FrequencyCounter.js
+++ FrequencyCounter.js
@@ -1,6 +1,10 @@
+// FREQUENCY COUNTER!!
+// Problem: You have 2 arrays, and you want to check if all of the elements in the first array are equal to the elements squared in the second array.
+// Order does not matter, but the frequency matters.. for example [1,2,4]should equal [1,4,16], but not [1,4,16,16]
 
-let arr1 = [1,3,5,9];
-let arr2 = [9,1,25,81];
+
+const arr1 = [1,3,5,9];
+const arr2 = [9,1,25,81];
 
 function arrayCheck(firstArray,secondArray){
 const obj1 = {};
@@ -17,9 +21,9 @@ for (let key in obj1){ // For every key, it checks to see if the key squared exi
 	if (!(key ** 2 in obj2)){
   return false
   }
-    if ((obj1[key]) !== obj2[key**2]){
+    if ((obj1[key]) !== obj2[key**2]){ // checks the value of the key in obj1 to see if its equal to the value of key squared in obj2. 
         return false
-    }           // checks the value of the key to see if its squared in obj2. 
+    }           
 }
 return true
 
